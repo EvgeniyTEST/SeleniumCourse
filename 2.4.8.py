e2e225f745d7b23457e8331acbeae19a60b13900
EvgeniyTEST <evgeniy-testt@yandex.ru>
2021-01-28 22:00:30 +0300

    add comment

--- 2.4.8.py
+++ 2.4.8.py
@@ -13,23 +13,21 @@ try:
     browser = webdriver.Chrome()
     link = "http://suninjuly.github.io/explicit_wait2.html"
     browser.get(link)
-
+# ждем $100
     button = WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
     book = browser.find_element_by_id('book')
     book.click()
-
+# считаем переменную
     iks = browser.find_element_by_css_selector("#input_value").text
     print(iks)
     x = int(iks)
     y = calc(x)
-
-    ANSWER = browser.find_element_by_css_selector('#answer')
-    ANSWER.send_keys(y)
-
+# ввод ответа
+    answer = browser.find_element_by_css_selector('#answer')
+    answer.send_keys(y)
+# клик Submit
     submit = browser.find_element_by_css_selector('#solve')
     submit.click()
 
-
 finally:
-
     browser.quit()
